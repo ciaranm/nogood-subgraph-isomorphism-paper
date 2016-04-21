745b8937c29b7cfedf8ae849773e8a3da9928e0f
Ciaran McCreesh <c.mccreesh.1@research.gla.ac.uk>
2016-04-21 11:22:16 +1000

    Start some non-working code

---
+++ code/simple.cc
@@ -0,0 +1,11 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include "simple.hh"
+
+auto simple_subgraph_isomorphism(const std::pair<Graph, Graph> &, const Params &) -> Result
+{
+    Result result;
+
+    return result;
+}
+
